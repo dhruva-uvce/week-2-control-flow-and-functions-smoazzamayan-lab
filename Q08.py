0d8db742f347fa723825562d5689376fca81cb9c
smoazzamayan-lab <smoazzamayan@gmail.com>
2026-04-12 15:10:31 +0530

    Update Q08.py

--- Q08.py
+++ Q08.py
@@ -7,3 +7,16 @@
 # Sample Output:  Sum of digits of 9876 = 30
 
 # --- YOUR CODE HERE ---
+n = input("Enter a number: ")
+l = len(n)
+n = int(n)
+i = 0
+dig = 0
+sum = 0
+temp = n
+while i < l:
+    dig = temp%10
+    sum = sum + dig
+    temp = int(temp/10)
+    i = i+1
+print(f"Sum of digits of {n} = {sum}")
